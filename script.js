f513cc00e8e95a91921f2e000486214050501a6d
tonydcdesign <97972011+tonydcdesign@users.noreply.github.com>
2022-04-14 12:21:26 -0400

    added files

---
+++ script.js
@@ -0,0 +1,7 @@
+const togglebutton = document.getElementsByClassName('toggle-button')[0]
+const navlinks = document.getElementsByClassName('nav-links')[0]
+
+togglebutton.addEventListener('click', () => {
+    navlinks.classList.toggle('active')
+
+})
